e707611b1211c886c4bdf28eb45596512596b075
superpuper777 <superfevrik@gmail.com>
2020-01-06 14:57:52 +0300

    Creating my first app and separating components

---
+++ src/Technologies.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <div>
+      <div> one div </div>
+      <div className="App">
+        <ul>
+          <li> css </li>
+          <li> html </li>
+          <li> js </li>
+          <li> react </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Technologies;
